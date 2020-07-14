383e80e357fc9009227af443e08bbb7692f8fa8b
Vignesh S, Manoj <Manoj.VigneshS@softwareag.com>
2020-07-14 10:40:19 +0530

    Added facebook pixel analytics

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import {MDXProvider} from '@mdx-js/react';
-
 import ThemeProvider from './theme/themeProvider';
 import mdxComponents from './mdxComponents';
 import Sidebar from './sidebar';
 import RightSidebar from './rightSidebar';
 import config from '../../config.js';
+import CookieConsent from 'react-cookie-consent';
 
 const Wrapper = styled('div')`
   display: flex;
   width: 100%;
   justify-content: space-between;
-  background: ${({ theme }) => theme.colors.background};
+  background: ${({theme}) => theme.colors.background};
 
   .sideBarUL li a {
-    color: ${({ theme }) => theme.colors.text};
+    color: ${({theme}) => theme.colors.text};
   }
 
   .sideBarUL .item > a:hover {
@@ -37,10 +37,10 @@ const Content = styled('main')`
   margin-right: 0px;
   padding-top: 2rem;
   // width: 860px;
-  background: ${({ theme }) => theme.colors.background};
+  background: ${({theme}) => theme.colors.background};
 
   table tr {
-    background: ${({ theme }) => theme.colors.background};
+    background: ${({theme}) => theme.colors.background};
   }
 
   @media only screen and (max-width: 1023px) {
@@ -71,25 +71,33 @@ const RightSideBarWidth = styled('div')`
   flex-shrink: 0;
 `;
 
-const Layout = ({ children, location }) => (
+const Layout = ({children, location}) => (
   <ThemeProvider location={location}>
     <MDXProvider components={mdxComponents}>
       <Wrapper>
         <LeftSideBarWidth className={'hiddenMobile'}>
-          <Sidebar location={location} />
+          <Sidebar location={location}/>
         </LeftSideBarWidth>
         {config.sidebar.title ? (
           <div
             className={'sidebarTitle sideBarShow'}
-            dangerouslySetInnerHTML={{ __html: config.sidebar.title }}
+            dangerouslySetInnerHTML={{__html: config.sidebar.title}}
           />
         ) : null}
         <Content>
           <MaxWidth>{children}</MaxWidth>
         </Content>
         <RightSideBarWidth className={'hiddenMobile'}>
-          <RightSidebar location={location} />
+          <RightSidebar location={location}/>
         </RightSideBarWidth>
+        <CookieConsent
+          enableDeclineButton
+          location="bottom"
+          buttonText="I understand and accept the use of cookies"
+          declineButtonText="I do not accept the use of cookies"
+          cookieName="gatsby-gdpr-facebook-pixel">
+          We are always working to improve this website for our users. To do this, we use the anonymous data provided by Cookies.
+        </CookieConsent>
       </Wrapper>
     </MDXProvider>
   </ThemeProvider>
